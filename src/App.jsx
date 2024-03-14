de476c9c6d6554cb804afab949d7caf00fbb9849
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-03-14 22:38:33 +0000

    added AuthPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
-  return <Button>hello world</Button>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/auth" element={<AuthPage />} />
+    </Routes>
+  );
 }
 
 export default App;
